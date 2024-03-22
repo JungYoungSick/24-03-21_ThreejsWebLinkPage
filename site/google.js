f07096400f30cee48f61c4b2916d02e386bb03b9
JungYoungSick <junfas1216@gmail.com>
2024-03-22 23:20:52 +0900

    익스프레스 설치 및 코드 작성(js 코드 연결필요)

---
+++ site/google.js
@@ -0,0 +1,37 @@
+function createInfospot(site, url, imagePath) {
+  const infospot = new PANOLENS.Infospot();
+  infospot.position.set(site.x, site.y, site.z);
+  infospot.addHoverText(site.name);
+
+  // 클릭 이벤트에 대한 콜백 함수
+  function clickCallback() {
+    window.location.href = url;
+  }
+
+  // 클릭 이벤트에 콜백 함수 등록
+  infospot.addEventListener("click", clickCallback);
+
+  // 이미지 경로가 제공된 경우 이미지를 변경합니다.
+  if (imagePath) {
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load(imagePath, function (texture) {
+      infospot.material.map = texture;
+      // Material이 변경되었음을 Three.js에 알립니다.
+      infospot.material.needsUpdate = true;
+    });
+  }
+
+  panorama.add(infospot);
+}
+
+// 사용 예시
+const google = {
+  x: 370,
+  y: 16,
+  z: -5000,
+  name: "구글 바로가기"
+};
+const googleUrl = "https://google.com";
+const googleImagePath = "image/Logo/google.jpeg";
+
+createInfospot(google, googleUrl, googleImagePath);
